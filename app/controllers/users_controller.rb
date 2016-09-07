317ebef38d53ada8f5a114c27fe811e92c6dbc20
Shizuna Ito <adfa1728@chiba-u.jp>
2016-09-07 17:52:03 +0900

    add sucsessaction (redirect user page and add flash)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -23,5 +24,5 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
         :password_confirmation)
     end
-    
+
 end
